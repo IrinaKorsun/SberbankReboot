7fefc76f86f4493156cc1818f23af2c27cb94fd5
Irina-Yushchenko <42397829+Irina-Yushchenko@users.noreply.github.com>
2019-10-21 12:07:41 +0700

    task2 (#57)
    * task2
    
    * task2 Calc yushchenko
    
    * Lesson2. Исправление task1  + task2.

---
+++ Lesson_02/src/main/java/yushchenko/l2_task01_yushchenko.java
@@ -0,0 +1,32 @@
+package main.java.yushchenko;
+
+import java.util.Scanner;
+
+public class l2_task01_yushchenko {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int lenArray = in.nextInt();
+        double[] myArray = new double[lenArray];
+        double minMyArray = 1;
+        double maxMyArray = 0;
+        double sum = 0;
+        double avvMyArray = 0;
+        System.out.println("Массив: ");
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = Math.random();
+            System.out.println(myArray[i]);
+            if (myArray[i] < minMyArray) {
+                minMyArray = myArray[i];
+            }
+            if (myArray[i] > maxMyArray) {
+                maxMyArray = myArray[i];
+            }
+            sum += myArray[i];
+        }
+        avvMyArray = sum / myArray.length;
+        System.out.println("Минимум: " + minMyArray);
+        System.out.println("Максимум: " + maxMyArray);
+        System.out.println("Среднее: " + avvMyArray);
+    }
+}
