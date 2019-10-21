63a1e3fa95e24f6cfe8513f2df4a2f0c732cc7d0
Chernoskutov Evgeny <37318930+ChernoskutovEM@users.noreply.github.com>
2019-10-21 17:24:41 +0700

    Feature 02 chernoskutov (#39)
    * Task 02 complete 20191019
    
    * Task 02 complete 20191019
    
    * task 02 BugFixed 20191021

---
+++ Lesson_02/src/main/java/chernoskutov/ArrRandom.java
@@ -0,0 +1,15 @@
+package main.java.chernoskutov;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ArrRandom {
+    private static final int SZ = 40;
+    public static void main(String[] args) {
+        System.out.println("All matches:");
+        Double[] arr = Stream.generate(Math::random).limit(SZ).sorted().peek(System.out::println).toArray(Double[]::new);
+        System.out.println("========");
+        System.out.println("min: " + Arrays.stream(arr).min(Double::compareTo).get().toString());
+        System.out.println("max: " + Arrays.stream(arr).max(Double::compareTo).get().toString());
+        System.out.println("avg: " + Arrays.stream(arr).reduce(Double::sum).get() / arr.length );
+    }
+}
---
+++ Lesson_02/src/main/java/chernoskutov/Calc.java
@@ -0,0 +1,78 @@
+package main.java.chernoskutov;
+
+import java.util.Scanner;
+/**
+ * Class <b>the Calculate</b>.
+ * @autor Chernoskutov Eugene
+ * @version 1.0
+ */
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== The program calculate ===");
+        while (true) {
+            double p1;
+            double p2;
+            String s;
+            System.out.println("To text 2 mun and select mathematics operation( + , -, / or *): ");
+            System.out.println("type first num:");
+            s = sc.nextLine();
+
+            if(isNumber(s)){
+                p1 = Double.parseDouble(s);
+            } else {
+                System.out.println("expect double.");
+                continue;
+            }
+            System.out.println("type second num:");
+            s = sc.nextLine();
+            if(isNumber(s)){
+                p2 = Double.parseDouble(s);
+            } else {
+                System.out.println("expect double.");
+                continue;
+            }
+            System.out.println("select operation( + , -, / or *):");
+            String oper = sc.next();
+            System.out.println("Result: ");
+            /**
+             * calculator method
+             * @param p1 - first argument
+             * @param p2 - second argument
+             * @param oper - operator
+             */
+            Double result = calcExecutor(p1, p2, oper);
+            if (result != null) {
+                System.out.println(result + "\n----------NEXT---------------");
+            }
+        }
+    }
+
+    private static boolean isNumber(String s){
+        return s.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    /**
+     * calculator method
+     * @return result of operation
+     */
+    private static Double calcExecutor(double p1, double p2, String oper) {
+        if (oper.equals("+")) {
+            return p1 + p2;
+        } else if (oper.equals("-")) {
+            return p1 - p2;
+        } else if (oper.equals("/")) {
+            if (p2 == 0) {
+                System.out.println("division by zero! ");
+                return null;
+            }
+            return p1 / p2;
+        } else if (oper.equals("*")) {
+            return p1 * p2;
+        } else {
+            System.out.println("check you input");
+        }
+        return null;
+    }
+}
