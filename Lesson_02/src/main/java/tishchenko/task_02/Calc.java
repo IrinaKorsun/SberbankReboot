4ed645eabcd5962761629cc82aab5457d2baddd3
Andrey <56593474+T-Trick@users.noreply.github.com>
2019-10-21 17:30:28 +0700

    feature-03-tishchenko(SimpleCalc) (#47)
    * Реализовал простой калькулятор с возможностью простых арифметических операции - сложение,вычитание,умножение,деление.
    Запуск происходит в цикле while, предоставляя пользователю возможность продолжить использование калькулятора после первого использования.
    
    * Инициализировал все переменные в самом начале, так как думаю это будет правильно, потому что JVM на них сразу выделяется память (если не прав, то поправь пожалуйста).
    Так же написал все переменые с маленькой буквы, в самом начале забыл про данное правило хорошего тона=)Учту в дальнейшем
    
    * Поправил форматирование с помощью Alt+ctrl+L

---
+++ Lesson_02/src/main/java/tishchenko/task_02/Calc.java
@@ -0,0 +1,41 @@
+package main.java.tishchenko.task_02;
+
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        String exit = "";
+        int firstNumb = 0;
+        int secondNumb = 0;
+        String action = "";
+        double secondNumbDouble = 0.0;
+        while (!exit.equals("n")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("----===FirstCalc!===----");
+            System.out.print("Enter FirstNumber: ");
+            firstNumb = scanner.nextInt();
+            System.out.print("Enter Action(+,-,*,/)");
+            action = scanner.next();
+            if (action.equals("exit")) {
+                break;
+            }
+            ;
+            System.out.print("Enter SecondNumber: ");
+            secondNumb = scanner.nextInt();
+            secondNumbDouble = (double) secondNumb;
+            if (action.equals("+")) {
+                System.out.println("Result : " + (firstNumb + secondNumb));
+            } else if (action.equals("-")) {
+                System.out.println("Result : " + (firstNumb - secondNumb));
+            } else if (action.equals("*")) {
+                System.out.println("Result : " + (firstNumb * secondNumb));
+            } else if (action.equals("/")) {
+                System.out.println("Result : " + (firstNumb / secondNumbDouble));
+            } else {
+                System.out.println("Error - Enter Incorrect action! Please enter the correct action");
+            }
+            System.out.println("Сontinue?(Enter 'y' to continue, 'n' to Exit");
+            exit = scanner.next();
+        }
+    }
+}
