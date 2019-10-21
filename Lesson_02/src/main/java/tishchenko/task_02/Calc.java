d9eb01ca2ce4ac4ed20c236490ec5127ec6faee4
t-trick <t-trick@mail.ru>
2019-10-21 13:22:43 +0700

    Поправил форматирование с помощью Alt+ctrl+L

--- Lesson_02/src/main/java/tishchenko/task_02/Calc.java
+++ Lesson_02/src/main/java/tishchenko/task_02/Calc.java
@@ -1,4 +1,5 @@
 package main.java.tishchenko.task_02;
+
 import java.util.Scanner;
 
 public class Calc {
@@ -7,25 +8,32 @@ public class Calc {
         int firstNumb = 0;
         int secondNumb = 0;
         String action = "";
-        double secondNumbDouble=0.0;
-        while (!exit.equals("n"))
-        {
+        double secondNumbDouble = 0.0;
+        while (!exit.equals("n")) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("----===FirstCalc!===----");
             System.out.print("Enter FirstNumber: ");
             firstNumb = scanner.nextInt();
             System.out.print("Enter Action(+,-,*,/)");
             action = scanner.next();
-            if (action.equals("exit")){break;};
+            if (action.equals("exit")) {
+                break;
+            }
+            ;
             System.out.print("Enter SecondNumber: ");
             secondNumb = scanner.nextInt();
-            secondNumbDouble = (double)secondNumb;
-            if (action.equals("+")){System.out.println("Result : " + (firstNumb+secondNumb));}
-            else if (action.equals("-")){System.out.println("Result : " + (firstNumb-secondNumb));}
-            else if (action.equals("*")){System.out.println("Result : " + (firstNumb*secondNumb));}
-            else if (action.equals("/")){System.out.println("Result : " + (firstNumb/secondNumbDouble));}
-            else
-                {System.out.println("Error - Enter Incorrect action! Please enter the correct action");}
+            secondNumbDouble = (double) secondNumb;
+            if (action.equals("+")) {
+                System.out.println("Result : " + (firstNumb + secondNumb));
+            } else if (action.equals("-")) {
+                System.out.println("Result : " + (firstNumb - secondNumb));
+            } else if (action.equals("*")) {
+                System.out.println("Result : " + (firstNumb * secondNumb));
+            } else if (action.equals("/")) {
+                System.out.println("Result : " + (firstNumb / secondNumbDouble));
+            } else {
+                System.out.println("Error - Enter Incorrect action! Please enter the correct action");
+            }
             System.out.println("Сontinue?(Enter 'y' to continue, 'n' to Exit");
             exit = scanner.next();
         }
