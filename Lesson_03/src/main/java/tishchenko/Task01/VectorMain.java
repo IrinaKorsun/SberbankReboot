a610f48e659a5ee7b2797a5260534d57040976f2
t-trick <t-trick@mail.ru>
2019-10-23 10:48:34 +0700

    Изменил подход в выполнению задания с использованием пройденной темы на Уроке 3

--- Lesson_03/src/main/java/tishchenko/Task01/VectorMain.java
+++ Lesson_03/src/main/java/tishchenko/Task01/VectorMain.java
@@ -1,68 +1,92 @@
 package tishchenko.Task01;
 
 class Vector {
+    double x, y, z;
 
-    public final double LengthVector(double x, double y, double z) {
-        double result = Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0) + Math.pow(z, 2.0));
+    // Создаем конструктор без входных параметров
+    Vector() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    // Создаем конструктор с входными параметрами
+    Vector(double newX, double newY, double newZ) {
+        this.x = newX;
+        this.y = newY;
+        this.z = newZ;
+    }
+
+    //Метод отрисовки вектора для проверки корректности
+    void PrintVector() {
+        System.out.println("(" + this.x + ";" + this.y + ";" + this.z + ")");
+    }
+
+    //Метод вычисляющий длину вектора
+    public double LengthVector(Vector vector) {
+        double result = Math.sqrt(Math.pow(vector.x, 2.0) + Math.pow(vector.y, 2.0) + Math.pow(vector.z, 2.0));
         return result;
     }
 
-    public final double ScalarMultiplication(double x1, double x2, double y1, double y2, double z1, double z2) {
-        double result = (x1 * x2) + (y1 * y2) + (z1 * z2);
+    //Метод вычисляющий калярное произведение 2х векторов
+    public double ScalarMultiplication(Vector vector) {
+        double result = (this.x * vector.x) + (this.y * vector.y) + (this.z * vector.z);
         return result;
     }
 
-    public final String VectorMultiplication(double x1, double x2, double y1, double y2, double z1, double z2) {
-        double new_x = (y1 * z2) - (z1 * y2);
-        double new_y = (z1 * x2) - (x1 * z2);
-        double new_z = (x1 * y2) - (y1 * x2);
+    //Метод вычисляющий произведение 2х векторов
+    public String VectorMultiplication(Vector vector) {
+        double new_x = (this.y * vector.z) - (this.z * vector.y);
+        double new_y = (this.z * vector.x) - (this.x * vector.z);
+        double new_z = (this.x * vector.y) - (this.y * vector.x);
         String result = "(" + (int) new_x + ";" + (int) new_y + ";" + (int) new_z + ")";
         return result;
     }
 
-    public final double SocVectors(double x1, double x2, double y1, double y2, double z1, double z2) {
-        Double result = (this.ScalarMultiplication(x1, x2, y1, y2, z1, z2)) / (this.LengthVector(x1, y1, z1) * this.LengthVector(x2, y2, z2));
+    //Метод вычисляющий угол между векторами
+    public double SocVectors(Vector vector) {
+        Double result = (this.ScalarMultiplication(vector)) / (this.LengthVector(vector) * this.LengthVector(vector));
         return result;
     }
 
-    public final String Sum(double x1, double x2, double y1, double y2, double z1, double z2) {
-        String result = "(" + (int) (x1 + x2) + ";" + (int) (y1 + y2) + ";" + (int) (z1 + z2) + ")";
+    //Метод вычисляющий сумму векторов
+    public String Sum(Vector vector) {
+        String result = "(" + (int) (this.x + vector.x) + ";" + (int) (this.y + vector.y) + ";" + (int) (this.z + vector.z) + ")";
         return result;
     }
 
-    public final String Def(double x1, double x2, double y1, double y2, double z1, double z2) {
-        String result = "(" + (int) (x1 - x2) + ";" + (int) (y1 - y2) + ";" + (int) (z1 - z2) + ")";
+    //Метод вычисляющий разность векторов
+    public String Def(Vector vector) {
+        String result = "(" + (int) (this.x - vector.x) + ";" + (int) (this.y - vector.y) + ";" + (int) (this.z - vector.z) + ")";
         return result;
     }
 
-    public void RandomArray(int n) {
-        String[] RandArray = new String[n];
+    //Метод формирующий массив из случайных N векторов, и возвращающий полученный массив векторов, на вход принимает длину массива N
+    public Vector[] RandomArray(int n) {
+        Vector[] RandArray = new Vector[n];
         for (int i = 0; i < n; i++) {
-            double x = Math.random();
-            double y = Math.random();
-            double z = Math.random();
-            String vector = "(" + x + ";" + y + ";" + z + ")";
-            RandArray[i] = vector;
-            System.out.println("RandomVector in Array position " + i + " - " + RandArray[i]);
+            RandArray[i] = new Vector(1 + Math.random() * 5, 1 + Math.random() * 5, 1 + Math.random() * 5);
+            System.out.print("Random Vector in " + i + " element Array = ");
+            RandArray[i].PrintVector();
         }
+        return RandArray;
     }
 }
 
 class VectorMain {
     public static void main(String[] args) {
-        Vector vector = new Vector();
-        double x1 = 2;
-        double y1 = 2;
-        double z1 = 2;
-        double x2 = 4;
-        double y2 = 6;
-        double z2 = 8;
-        System.out.println("Length vector - " + vector.LengthVector(x1, y1, z1));
-        System.out.println("Scalar product of vectors - " + vector.ScalarMultiplication(x1, x2, y1, y2, z1, z2));
-        System.out.println("Vector multiplication - " + vector.VectorMultiplication(x1, x2, y1, y2, z1, z2));
-        System.out.println("Cosine of angle between vectors - " + vector.SocVectors(x1, x2, y1, y2, z1, z2));
-        System.out.println("The sum of the vectors - " + vector.Sum(x1, x2, y1, y2, z1, z2));
-        System.out.println("The difference of the vectors - " + vector.Def(x1, x2, y1, y2, z1, z2));
-        vector.RandomArray(3);
+        Vector vectorOne = new Vector(2, 4, 6);
+        System.out.print("Vector One -");
+        vectorOne.PrintVector();
+        Vector vectorTwo = new Vector(3, 5, 7);
+        System.out.print("Vector Two -");
+        vectorTwo.PrintVector();
+        System.out.println("Length vector - " + vectorOne.LengthVector(vectorOne));
+        System.out.println("Scalar product of vectors - " + vectorOne.ScalarMultiplication(vectorTwo));
+        System.out.println("Vector multiplication - " + vectorOne.VectorMultiplication(vectorTwo));
+        System.out.println("Cosine of angle between vectors - " + vectorOne.SocVectors(vectorTwo));
+        System.out.println("The sum of the vectors - " + vectorOne.Sum(vectorTwo));
+        System.out.println("The difference of the vectors - " + vectorOne.Def(vectorTwo));
+        vectorOne.RandomArray(3);
     }
 }
