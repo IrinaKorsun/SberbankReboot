1794def58b378b991c0a6ab7a00bb3a91c40e997
IrinaKorsun <irina.korsun.nsu@gmail.com>
2019-10-24 16:22:48 +0300

    qqqq

--- Lesson_01/src/main/java/galliamov/Task_01.java
+++ Lesson_01/src/main/java/galliamov/Task_01.java
@@ -2,6 +2,7 @@ package galliamov;
 
 public class Task_01 {
     public static void main(String[] args) {
+        Task_01 t = new Task_01();
         System.out.println("Hello world!");
     }
 }
--- Lesson_04/src/main/java/kuzmina/Task_01.java
+++ Lesson_04/src/main/java/kuzmina/Task_01.java
@@ -1,4 +1,8 @@
 package main.java.kuzmina;
 
 public class Task_01 {
+    aaaa
+
+
+    hdasjkdhasj
 }
