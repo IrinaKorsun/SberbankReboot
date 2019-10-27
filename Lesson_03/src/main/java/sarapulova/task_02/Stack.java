9bb0b6ac049449d89004625b24f0e88bdb66768d
Ann <ancha.lynx@gmail.com>
2019-10-27 16:05:59 +0700

    feature-03-sarapulova stack, queue, vector (#80)
    * feature-03-sarapulova stack, queue, vector
    
    * bugfix-03-sarapulova format code

---
+++ Lesson_03/src/main/java/sarapulova/task_02/Stack.java
@@ -0,0 +1,47 @@
+package sarapulova.task_02;
+
+public class Stack {
+    int arrSize;
+    int[] arrStack;
+    int topStack;
+
+    Stack(int cnt) {
+        arrSize = cnt;
+        arrStack = new int[arrSize];
+        topStack = -1;
+    }
+
+    private boolean isEmptyStack() {
+        return (topStack == -1);
+    }
+
+    private boolean isFullStack() {
+        return (topStack == arrSize - 1);
+    }
+
+    private void Push(int element) {
+        arrStack[++topStack] = element;
+    }
+
+    private int Pop() {
+        return arrStack[topStack--];
+    }
+
+    public static void main(String[] args) {
+        Stack mStack = new Stack(5);
+        int value;
+
+        System.out.print("Заполняем Стек: \n");
+        while (!mStack.isFullStack()) {
+            value = (int) (Math.random() * 100);
+            System.out.print(value + "\n");
+            mStack.Push(value);
+        }
+
+        System.out.print("Читаем Стек: \n");
+        while (!mStack.isEmptyStack()) {
+            value = mStack.Pop();
+            System.out.print(value + "\n");
+        }
+    }
+}
