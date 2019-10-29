be9ff6eb2c1dc7061ea84f024ef2eba52c0201a6
VitRad <vitrad300@gmail.com>
2019-10-29 12:06:07 +0700

    Lesson05_Task05_Singletone_by_Radionov

--- Lesson_05/src/main/java/radionov/Sun.java
+++ Lesson_05/src/main/java/radionov/Sun.java
@@ -2,9 +2,12 @@ package radionov;
 
 public class Sun {
     private static Sun instance;
-    private Sun(){}
-    public static Sun getInstance(){
-        if(instance == null){
+
+    private Sun() {
+    }
+
+    public static Sun getInstance() {
+        if (instance == null) {
             instance = new Sun();
         }
         return instance;
