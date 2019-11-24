5d1ecf6b1c42d3ce82a81bbd52b999f0dd4c8467
Вера <o.vorona6@yandex.ru>
2019-11-24 10:42:04 +0700

    ищем дубли в коллекциях - изменения

--- Lesson_05/src/main/java/ostrikova/task_01/Demo.java
+++ Lesson_05/src/main/java/ostrikova/task_01/Demo.java
@@ -6,18 +6,17 @@ import java.util.*;
 
 //моё сочинение
 public class Demo {
-// я не знала, что можно рандомом текст создавать)) теперь буду знать)) оставила свой вариант
 
-    public static ArrayList<UserSber> createCollection() {  //создаём коллекцию
+
+    private static List<UserSber> createCollection() {
         //  создаём коллекцию 1
-        ArrayList<UserSber> user = new ArrayList<>();
+        List<UserSber> user = new ArrayList<>();
 
         for (int i = 1; i <= 100_000; i++) {
             UserSber UserSber = new UserSber();
 
             Integer x = 7000 + (int) (Math.random() * 100000);
             UserSber.setUsername(x.toString());
-            String n = x.toString();
 
             Integer e = 10000 + (int) (Math.random() * 10000000);
             UserSber.setEmail(e.toString());
@@ -26,7 +25,7 @@ public class Demo {
             for (int j = 0; j < 5; j++) {
                 p[j] = (int) (Math.random() * 9);
                 String p1 = String.valueOf(p[j]);
-
+                UserSber.setPasswordHash(p1);
             }
             user.add(UserSber);
         }
@@ -34,19 +33,21 @@ public class Demo {
     }
 
     public static void main(String[] args) {
-        ArrayList<UserSber> createColl1 = createCollection();
-        ArrayList<UserSber> createColl2 = createCollection();
+        List<UserSber> createColl1 = createCollection();
+        List<UserSber> createColl2 = createCollection();
         List<UserSber> duplicates = findDuplicates(createColl1, createColl2);
         List<UserSber> duplicates2 = findDuplicates(createColl1, createColl2);
     }
-    //пишем метод для поиска дублей - это ваш вариант
-    public static List<UserSber> findDuplicates(ArrayList<UserSber> collA, ArrayList<UserSber> collB) {
+
+    //пишем метод для поиска дублей
+    private static List<UserSber> findDuplicates(List<UserSber> collA, List<UserSber> collB) {
         List<UserSber> duplicates = new ArrayList<>();
         long start = System.currentTimeMillis();
         for (UserSber firstSber : collA) {
             for (UserSber secondSber : collB) {
                 if (firstSber.equals(secondSber)) {
                     duplicates.add(firstSber);
+                    System.out.println(firstSber);
                 }
             }
         }
@@ -62,24 +63,20 @@ public class Demo {
         List<UserSber> duplicates2=new ArrayList<>();           //коллекция для дублей
         ArrayList <UserSber> allElem=new ArrayList<>();     //создаём новую коллекцию, где будут объеденены две коллекции
         long start = System.currentTimeMillis();
-        for (int i = 0; i <= 20; i++) {
+        for (int i = 0; i <= 100_000; i++) {
             allElem.add(coll1.get(i)); // добавляем элементы из коллекции 1
         }
         for (int k = 0; k <= 20; k++) {
             allElem.add(coll2.get(k)); // добавляем элементы из коллекции 1
         }
 
-        Set<UserSber> set = new HashSet<UserSber>() {
-            @Override
-            public boolean add(UserSber e) {
-                if (contains(e)) {
-                    duplicates2.add(e);
-                }
-                return super.add(e);
-            }
-        };
+        Set<UserSber> set = new HashSet<UserSber>();
+
         for (UserSber t : allElem) {
-            set.add(t);
+            //здесь пошли изменения
+            if (allElem.contains(t)) {
+                set.add(t);}
+            System.out.println(t);
         }
         long end = System.currentTimeMillis();
         NumberFormat formatter = new DecimalFormat("#0.00000");
@@ -88,5 +85,3 @@ public class Demo {
     }
 
 }
-
-
