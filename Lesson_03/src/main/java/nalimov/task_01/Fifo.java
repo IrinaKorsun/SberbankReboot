03274afdf287eb94e903aab74b948fb94e7d987b
andreynalimov <andreynalimov@yandex.ru>
2019-11-26 20:34:18 +0700

    nalimov-edit-fifo-print-add-factory

--- Lesson_03/src/main/java/nalimov/task_01/Fifo.java
+++ Lesson_03/src/main/java/nalimov/task_01/Fifo.java
@@ -9,28 +9,26 @@ public class Fifo {
     int queTail = 0;
     //Начало очереди
     int queHear = 0;
-
     //Забираем из очереди
     public int queGet() {
         return this.queArray[this.queHear++] = 0;
     }
-
     //Добавляем в очередь
     public int quePut(int x) {
         return this.queArray[this.queTail++] = x;
     }
-
     //Проверка конца массива
     public boolean getQueArrayLength() {
         return this.queTail == this.queArray.length;
     }
-
     public void setQueTail(int quePointer) {
         this.queTail = quePointer;
     }
-
     public void setQueHear(int queHear) {
         this.queHear = queHear;
+    }
+    public void printArray() {
+        System.out.println(Arrays.toString(this.queArray));
     }
 
     //Добавляем, в очередь из 4х ячеек, 110 элементов, при заполнении вычитываем
@@ -38,22 +36,22 @@ public class Fifo {
         Fifo fifo = new Fifo();
         for (int i = 0; i <= 5; i++) {
             fifo.quePut(i);
-            System.out.println("quePut " + Arrays.toString(fifo.queArray));
+            fifo.printArray();
             if (fifo.getQueArrayLength()) {
                 for (int a = 0; a < fifo.queTail; a++) {
                     fifo.queGet();
-                    System.out.println("queGet " + Arrays.toString(fifo.queArray));
+                    fifo.printArray();
                 }
-                System.out.println("----------------------------------");
                 fifo.setQueHear(0);
                 fifo.setQueTail(0);
             }
         }
         for (int a = 0; a < fifo.queTail; a++) {
             fifo.queGet();
-            System.out.println("queGet " + Arrays.toString(fifo.queArray));
+            fifo.printArray();
         }
 
     }
+
 
 }
