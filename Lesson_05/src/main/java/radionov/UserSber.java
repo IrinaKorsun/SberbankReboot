95de71fc58b0b2e721f18dc32592fffa113bb45b
VitRad <vitrad300@gmail.com>
2019-11-28 14:13:20 +0700

    feature-Lesson5_byRadionov

---
+++ Lesson_05/src/main/java/radionov/UserSber.java
@@ -0,0 +1,58 @@
+package radionov;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class UserSber {
+    private String username;
+    private String email;
+    private byte[] passwordHash;
+
+    public UserSber(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = username.getBytes();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte[] getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(byte[] passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserSber userSber = (UserSber) o;
+        return username.equals(userSber.username) &&
+                email.equals(userSber.email) &&
+                Arrays.equals(passwordHash, userSber.passwordHash);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(username, email);
+        result = 31 * result + Arrays.hashCode(passwordHash);
+        return result;
+    }
+}
+
