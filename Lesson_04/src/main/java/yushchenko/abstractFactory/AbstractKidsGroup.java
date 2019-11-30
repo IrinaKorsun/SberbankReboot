3778831885383ae94ea9d449bc980bcadfbbbe6e
shevninka1974 <4a flarm v>
2019-11-30 15:27:47 +0700

    Задание урока 5. Поиск дубликатов Затраченное вемя на поиск : 358 milisec.

---
+++ Lesson_04/src/main/java/yushchenko/abstractFactory/AbstractKidsGroup.java
@@ -0,0 +1,11 @@
+package yushchenko.abstractFactory;
+
+public abstract class AbstractKidsGroup {
+    public abstract Educator createEducator();
+
+    public abstract Nanny createNanny();
+
+    public abstract Children createChildren();
+}
+
+
