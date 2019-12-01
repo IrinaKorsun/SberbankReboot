c833aed2a7884ade7288c4dc9f992795c05805f9
IrinaKorsun <irina.korsun.nsu@gmail.com>
2019-12-01 15:32:10 +0300

    удалено лишнее, измененен путь до файла

--- Lesson_06/src/main/java/ostrikova/Info.java
+++ Lesson_06/src/main/java/ostrikova/Info.java
@@ -1,7 +1,5 @@
 package ostrikova;
 
-import sun.applet.Main;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,35 +7,22 @@ import java.net.URLClassLoader;
 import java.util.Arrays;
 
 public class Info {
-           File file = new File("Lesson_06/lib/kuzmina");
-            URL url = file.toURI().toURL();
-            URL[] urls = new URL[]{url};
-            ClassLoader cl = new URLClassLoader(urls);
-            Class cls = cl.loadClass("kuzmina.Main");
 
-    public Info() throws MalformedURLException, ClassNotFoundException {
+
+    public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
+
+        File file = new File("Lesson_06/lib");
+        URL url = file.toURI().toURL();
+        URL[] urls = new URL[]{url};
+        ClassLoader cl = new URLClassLoader(urls);
+        Class cls = cl.loadClass("kuzmina.Main");
+
+        System.out.println("Имя класса: " + cls);
+        System.out.println("Поля класса: " + Arrays.toString(cls.getDeclaredFields()));
+        System.out.println("Родительский класс: " + cls.getSuperclass());
+        System.out.println("Методы класса: " + Arrays.toString(cls.getDeclaredMethods()));
+        System.out.println("Конструкторы класса: " + Arrays.toString(cls.getConstructors()));
     }
 
-   public static class CodeAnalyzer {
-
-        public static void analyzeClass(Object o) throws MalformedURLException, ClassNotFoundException {
-            File file = new File("Lesson_06/lib/kuzmina");
-            URL url = file.toURI().toURL();
-            URL[] urls = new URL[]{url};
-            ClassLoader cl = new URLClassLoader(urls);
-            Class cls = cl.loadClass("kuzmina.Main");
-
-            System.out.println("Имя класса: " + cls);
-            System.out.println("Поля класса: " + Arrays.toString(cls.getDeclaredFields()));
-            System.out.println("Родительский класс: " + cls.getSuperclass());
-            System.out.println("Методы класса: " +  Arrays.toString(cls.getDeclaredMethods()));
-            System.out.println("Конструкторы класса: " + Arrays.toString(cls.getConstructors()));
-        }
-
-        public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
-
-            analyzeClass(new Main());
-        }
-    }
 
 }
