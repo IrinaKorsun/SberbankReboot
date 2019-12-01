549f0cd8afdf30c8fd525f6a2b7cbe401596a7c9
shevninka1974 <4a flarm v>
2019-12-01 21:03:54 +0700

    Задание урока 6. Запуск метода через Reflections.

--- Lesson_06/src/main/java/shevnin/ShevninReflection.java
+++ Lesson_06/src/main/java/shevnin/ShevninReflection.java
@@ -1,10 +1,31 @@
 package shevnin;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class ShevninReflection {
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    public static void main(String[] args) throws MalformedURLException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+       File file = new File("\\lib\\kuzmina");
+        URL url = file.toURI().toURL();
+        URL[] urls = new URL[]{url};
+        ClassLoader cl = new URLClassLoader(urls);
+        Class kuzminaMain = cl.loadClass("kuzmina.Main");
+        System.out.println("Found Class - " + kuzminaMain.getName() + "!");
+        for (Method method : kuzminaMain.getDeclaredMethods()) {
+            int modifiers = method.getModifiers();
+            if (Modifier.isStatic(modifiers)) {
+                if (Modifier.isPrivate(modifiers)) method.setAccessible(true);
+                if (method.getParameterTypes().length == 0) {
+                    System.out.println("Call method \"" + method.getName() + "\". This is a static method and has no parameters");
+                    method.invoke(null, null);
+                 }
+            }
+        }
     }
 
 }
