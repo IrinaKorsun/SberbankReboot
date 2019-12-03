6898a9fddd44cfe7a575178066f0b94ae48f524b
Вера <o.vorona6@yandex.ru>
2019-12-03 19:25:07 +0700

    рефлексия - исправление

--- Lesson_06/src/main/java/ostrikova/Ref.java
+++ Lesson_06/src/main/java/ostrikova/Ref.java
@@ -9,12 +9,7 @@ import java.net.URLClassLoader;
 import java.util.Arrays;
 
 public class Ref {
-    public static void printColour(Object Main) throws MalformedURLException, ClassNotFoundException {
-        File file = new File("Lesson_06/lib");
-        URL url = file.toURI().toURL();
-        URL[] urls = new URL[]{url};
-        ClassLoader cl = new URLClassLoader(urls);
-        Class cls = cl.loadClass("kuzmina.Main");
+    public static void printColour(Object Main, Class cls) throws MalformedURLException, ClassNotFoundException {
         try {
             Method printColour = cls.getDeclaredMethod("printColour"); // получаем метод по имени
             printColour.setAccessible(true); //делаем метод доступным
@@ -31,7 +26,7 @@ public class Ref {
         ClassLoader cl = new URLClassLoader(urls);
         Class cls = cl.loadClass("kuzmina.Main");
 
-        printColour(cls);
+        printColour(cls, cls);
 
         System.out.println("Информация о классе:");
         System.out.println("Имя класса: " + cls);
