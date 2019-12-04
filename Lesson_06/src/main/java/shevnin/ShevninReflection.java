f9acc69fb118e793deff3c5446a4de5140b11026
shevninka1974 <4a flarm v>
2019-12-04 23:22:48 +0700

    Задание урока 6. Установлена обработка исключений

--- Lesson_06/src/main/java/shevnin/ShevninReflection.java
+++ Lesson_06/src/main/java/shevnin/ShevninReflection.java
@@ -9,23 +9,30 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class ShevninReflection {
-    public static void main(String[] args) throws MalformedURLException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
-       File file = new File("\\lib\\kuzmina");
-        URL url = file.toURI().toURL();
-        URL[] urls = new URL[]{url};
-        ClassLoader cl = new URLClassLoader(urls);
-        Class kuzminaMain = cl.loadClass("kuzmina.Main");
-        System.out.println("Found Class - " + kuzminaMain.getName() + "!");
-        for (Method method : kuzminaMain.getDeclaredMethods()) {
-            int modifiers = method.getModifiers();
-            if (Modifier.isStatic(modifiers)) {
-                if (Modifier.isPrivate(modifiers)) method.setAccessible(true);
-                if (method.getParameterTypes().length == 0) {
-                    System.out.println("Call method \"" + method.getName() + "\". This is a static method and has no parameters");
-                    method.invoke(null, null);
-                 }
+    public static void main(String[] args) throws MalformedURLException, InvocationTargetException, IllegalAccessException {
+        String classPath = "Lesson_06/lib";
+        String className = "kuzmina.Main";
+        try {
+            File file = new File(classPath);
+            URL url = file.toURI().toURL();
+            URL[] urls = new URL[]{url};
+            ClassLoader cl = new URLClassLoader(urls);
+            Class someClass = cl.loadClass(className);
+            System.out.println("Найден класс - " + someClass.getName());
+            for (Method method : someClass.getDeclaredMethods()) {
+                int modifiers = method.getModifiers();
+                if (Modifier.isStatic(modifiers)) {
+                    if (Modifier.isPrivate(modifiers)) method.setAccessible(true);
+                    if (method.getParameterTypes().length == 0) {
+                        System.out.println("Вызов метода \"" + method.getName() + "\". Это статический метод без параметров");
+                        method.invoke(null, null);
+                    }
+                }
             }
+        } catch (NoClassDefFoundError e) {
+            System.out.println("Неправильное наименование класса \"" + className + "\"");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Класс \"" + className + "\" не найден");
         }
     }
-
 }
